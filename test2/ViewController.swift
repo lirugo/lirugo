1a0bc52eeb8a504e2bc23625ff59071e02a43b7b
user <user@macbook-air-xuser.local>
2020-03-30 12:43:52 +0300

    Initial Commit

---
+++ test2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by user on 3/30/20.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
