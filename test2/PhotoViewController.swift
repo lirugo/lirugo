712aeedf92284b18fe5918d68bf6c0cf9dea0503
user <user@macbook-air-xuser.local>
2020-03-31 14:33:48 +0300

    Init video full screen

---
+++ test2/PhotoViewController.swift
@@ -0,0 +1,39 @@
+//
+//  PhotoViewController.swift
+//  test2
+//
+//  Created by user on 3/30/20.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+
+    var takenPhoto:UIImage?
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let availableImage = takenPhoto {
+            imageView.image = availableImage
+        }
+    }
+    
+    @IBAction func goBack(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- test2/ViewController.swift
+++ test2/ViewController.swift
@@ -7,14 +7,80 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    let captureSession = AVCaptureSession()
+    var previewLayer:CALayer!
+    
+    var captureDevice:AVCaptureDevice!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        prepareCamera()
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
+    func prepareCamera(){
+        captureSession.sessionPreset = AVCaptureSession.Preset.photo
+        
+        if let availableDevices = AVCaptureDevice.DiscoverySession(
+            deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .front
+        ).devices as [AVCaptureDevice]? {
+                captureDevice = availableDevices.first
+            beginSession()
+        }
     }
 
+    func beginSession(){
+        do {
+            let captureDeviceInput = try AVCaptureDeviceInput(device: captureDevice)
+            captureSession.addInput(captureDeviceInput)
+        }catch {
+            print(error.localizedDescription)
+        }
+        
+        if let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession) as AVCaptureVideoPreviewLayer?{
+            previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+            self.previewLayer = previewLayer
+            self.view.layer.addSublayer(self.previewLayer)
+            self.previewLayer.frame = self.view.layer.frame
+            
+            captureSession.startRunning()
+            
+            let dataOutput = AVCaptureVideoDataOutput()
+            dataOutput.videoSettings =
+                [
+                    (kCVPixelBufferPixelFormatTypeKey as NSString) :
+                    NSNumber(value:kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)
+                ] as [String:Any]
+            
+            dataOutput.alwaysDiscardsLateVideoFrames = true
+            
+            if captureSession.canAddOutput(dataOutput){
+                captureSession.addOutput(dataOutput)
+            }
+            
+            captureSession.commitConfiguration()
+            
+        }
+        
+    }
 
 }
 
